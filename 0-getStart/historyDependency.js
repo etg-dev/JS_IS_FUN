0601cdacbbaae40b84bb910a68adc3260eb713b1
etg-dev <taghinia.dev@gmail.com>
2023-01-07 15:09:27 +0330

    initial commit

---
+++ 0-getStart/historyDependency.js
@@ -0,0 +1,127 @@
+/* 
+*Fix the cause, not the symptom ;) 
+!Steve Maguire
+
+Notice: All this information at history section is taken from Wikipedia and home.CERN**
+
+?History of the World Wide Web
+
+      The World Wide Web (WWW) is a global information medium which users can access via computers connected to the Internet
+
+      Tim Berners-Lee invented the World Wide Web while working at CERN in 1989. He proposed a "universal linked information system" using several concepts and technologies
+
+      He developed the first web server, the first web browser, and a document formatting protocol, called Hyper Text Markup Language (HTML)
+
+      for public internet use in 1993, many web browsers were soon developed, with Marc Andreessen's Mosaic (later Netscape Navigator), being particularly easy to use and install,
+
+
+      Websites for use by the general public began to emerge in 1994. This spurred competition in server and browser software, highlighted in the Browser wars which was initially dominated by Netscape Navigator and Internet Explorer.
+
+?HTML CSS JS 
+
+      The features of HTML evolved over time, leading to HTML version 2 in 1995, HTML3 and HTML4 in 1997, and HTML5 in 2014. The language was extended with advanced formatting in Cascading Style Sheets (CSS) and with programming capability by JavaScript
+
+?JAVA SCRIPT History
+
+      JavaScript often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript
+      on the client side for webpage behavior, often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users' devices.
+
+      !Creation at Netscape **
+
+      The first popular web browser with a graphical user interface, Mosaic, was released in 1993. Accessible to non-technical people, it played a prominent role in the rapid growth of the nascent World Wide Web. The lead developers of Mosaic then founded the Netscape corporation, which released a more polished browser, Netscape Navigator, in 1994. This quickly became the most-used.
+
+      During these formative years of the Web, web pages could only be static, lacking the capability for dynamic behavior after the page was loaded in the browser. There was a desire in the flourishing web development scene to remove this limitation, so in 1995, Netscape decided to add a scripting language to Navigator. They pursued two routes to achieve this: collaborating with Sun Microsystems to embed the Java programming language, while also hiring Brendan Eich to embed the Scheme language.
+
+      *Netscape management soon decided that the best option was for Eich to devise a new language, with syntax similar to Java and less like Scheme or other extant scripting languages.Although the new language and its interpreter implementation were called LiveScript when first shipped as part of a Navigator beta in September 1995, the name was changed to JavaScript for the official release in December.
+
+      !Adoption by Microsoft :)
+
+      Microsoft debuted Internet Explorer in 1995, leading to a browser war with Netscape. On the JavaScript front, Microsoft reverse-engineered the Navigator interpreter to create its own, called JScript.
+
+      JScript was first released in 1996, alongside initial support for CSS and extensions to HTML. Each of these implementations was noticeably different from their counterparts in Navigator.
+
+      The rise of JScript **
+
+      In November 1996, Netscape submitted JavaScript to Ecma International, as the starting point for a standard specification that all browser vendors could conform to. This led to the official release of the first ECMAScript language specification in June 1997.
+      The standards process continued for a few years, with the release of ECMAScript 2 in June 1998 and ECMAScript 3 in December 1999. Work on ECMAScript 4 began in 2000.
+
+?Get start with JS
+*ref : https://www.programiz.com/javascript
+       *https://www.w3schools.com/js/
+
+
+      *you can run JavaScript in several ways
+      !1.Using console tab of web browsers
+      !2.Using Node.js
+      !3.By creating web pages
+
+      ?1. Using Console Tab of Web Browsers
+      1.Open your favorite browser (highly recommended using Google Chrome).
+      *link for download:https://www.google.com/chrome/?brand=BNSD&gclid=Cj0KCQiA5NSdBhDfARIsALzs2EBaDWc58_kMa25eo4Ym_9kjzAwQLGu1btFa4wq8Q4pPdl8vhxl1O2caAsm6EALw_wcB&gclsrc=aw.ds
+      
+      2.Open the developer tools by right clicking on an empty area and select Inspect. Shortcut:
+      
+      3.On the developer tools, go to the console tab. Then, write JavaScript code and press enter to run the code.
+
+      ?2.Using Node.js
+      Node is a back-end run-time environment for executing JavaScript code. To run JS using Node.js, follow these steps:
+      *link for download:https://nodejs.org/en/download/
+      !please download LTS(Long Term Support) version
+
+      1.Install the latest version of Node.js.
+      !for ensure node.js installed properly go to command propmt and write node -v if version of node printed node js installed properly
+      
+      2.Install an IDE/Text Editor like Visual Studio Code. In VS code => create file and write some js codes
+      *link for download:https://code.visualstudio.com/download
+
+      3.Open up the terminal/command prompt , navigate to the file location , type node nameFile.js and hit enter.
+
+      ?3.Creating Web Pages
+      JavaScript was initially created to make web pages interactive, that's why JavaScript and HTML go hand in hand. To run JS from a webpage, follow these steps:
+      
+      1.Open VS Code > Go to File > New File > Save it with .html extension. For example
+
+      2.Copy this doctype (minimum valid HTML code) and save it in the file.
+      <!doctype html>
+      <html lang="en">
+      <head>
+      <meta charset="utf-8">
+
+      <title>Programiz</title>
+      </head>
+
+      <body>
+      <script src="nameFile.js"></script>
+      </body>
+      </html>
+
+      3.Similarly create a JS file, write the following JS code and save it with .js extension like main.js.
+      console.log('hello world');
+
+      4.Open the main.html file using a browser.
+
+      5.To check if our JS code ran or not, Right click on the web page > Inspect > Choose console tab.
+
+?Which extensions do I use VSCode?
+      1.Darcula Theme
+      2.Better Comment
+      3.HTML BOILERPLATE
+      4.HTML CSS SUPPORT
+      5.IntelliCode
+      6.JavaScirpt (ES6) code snippets
+      7.live Server
+      8.Material Icon
+      9.Multiple cursor case
+      10.Prettier - Code
+
+?references
+      1.https://www.programiz.com/javascript
+      2.https://www.w3schools.com/js/
+      3.https://javascript.info/
+
+      !Also, if you have any problem, you can surely find the solution on this website
+      4.https://stackoverflow.com/questions/tagged/javascript
+
+*All the source codes that we will learn in this course are available on my github after each session
+      5.https://github.com/etg-dev/JS_IS_FUN
+*/
