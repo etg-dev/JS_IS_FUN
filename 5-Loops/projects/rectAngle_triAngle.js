2a197d2b8c92e71ef95b2ef39bc5bc60e0986d9f
etg-dev <taghinia.dev@gmail.com>
2023-01-25 22:42:26 +0330

    nested loop & create rectAngle

---
+++ 5-Loops/projects/rectAngle_triAngle.js
@@ -0,0 +1,17 @@
+console.log("////////rectAngle");
+const height = 4;
+for (let i = 0; i < height; i++) {
+  for (let j = 0; j < i + 1; j++) {
+    console.log("*");
+  }
+  console.log("\n");
+}
+
+console.log("////////triAngle");
+const width = 5;
+for (let i = 0; i < height; i++) {
+  for (let j = 0; j < width; j++) {
+    console.log("*");
+  }
+  console.log("\n");
+}
