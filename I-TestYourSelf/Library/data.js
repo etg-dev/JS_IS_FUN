926a96f113f1024cbe5b62c3631ea3bc02f188d0
etg-dev <taghinia.dev@gmail.com>
2023-02-20 11:25:41 +0330

    Project implementation requirements with mock data

---
+++ I-TestYourSelf/Library/data.js
@@ -0,0 +1,41 @@
+export const members = [
+  {
+    fName: "Erfan",
+    lName: "Taghinia",
+    nCode: "0926068090",
+    memberType: "AUTHOR",
+  },
+  {
+    fName: "Ali",
+    lName: "Hoseini",
+    nCode: "0926067060",
+    memberType: "AUTHOR",
+  },
+  {
+    fName: "Mohammad",
+    lName: "Javadi",
+    nCode: "0926063055",
+    memberType: "AUTHOR",
+  },
+];
+
+export const books = [
+  {
+    name: "Clean Code",
+    price: "15$",
+    publish: new Date("01/03/1999"),
+    authors: "Robert C.Martin",
+  },
+  {
+    name: "Code Complete (2 Edition)",
+    price: "13$",
+    publish: new Date("02/09/2003"),
+    authors: "Steve McConnell",
+  },
+  {
+    name: "Design Patterns GoF",
+    price: "22$",
+    publish: new Date("01/01/2000"),
+    authors: ["Erich Gamma", "Richard Helm", "Ralph Johnson", "John Vlissides"],
+  },
+];
