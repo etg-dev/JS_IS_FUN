a6645f1d988890dd8b5c88217507100ac40594bb
etg-dev <taghinia.dev@gmail.com>
2023-03-06 13:12:20 +0330

    Make queue with array

---
+++ L-Queue/queueArray.js
@@ -0,0 +1,74 @@
+//FIFO -> First in first out
+//What we need ?
+// 1. size()
+// 2. add(v)
+// 3. take()
+// 4. head()
+// 5. tail()
+// 6. print()
+
+class Queue {
+  #list;
+
+  constructor() {
+    this.#list = [];
+  }
+
+  size() {
+    return this.#list.length;
+  }
+
+  add(v) {
+    this.#list.push(v);
+  }
+
+  take() {
+    if (this.size() === 0) return null;
+
+    this.#list.shift();
+
+    return true;
+  }
+
+  head() {
+    if (this.size() === 0) return null;
+
+    return this.#list[0];
+  }
+
+  tail() {
+    if (this.size() === 0) return null;
+
+    const lastElement = this.size() - 1;
+
+    return this.#list[lastElement];
+  }
+
+  print() {
+    if (this.size() === 0) return null;
+
+    this.#list.map((element) => {
+      console.log(element);
+    });
+  }
+}
+
+const queue = new Queue();
+
+queue.add(1);
+queue.add(2);
+queue.add(3);
+
+const length = queue.size();
+
+const head = queue.head();
+
+const tail = queue.tail();
+
+console.log(`Size of the list : ${length}`);
+console.log(`Top element is  : ${head}`);
+console.log(`Last element is  : ${tail}`);
+
+queue.take();
+
+queue.print();
