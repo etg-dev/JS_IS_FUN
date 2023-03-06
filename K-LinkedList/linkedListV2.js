81b1b6bc86f0271bf74f71c98b29b4cf327500c4
etg-dev <taghinia.dev@gmail.com>
2023-03-06 13:10:58 +0330

    Final linked list v2

--- K-LinkedList/linkedListV2.js
+++ K-LinkedList/linkedListV2.js
@@ -29,8 +29,8 @@ class Node {
 }
 
 class LinkedList {
-  #head = null;
-  #tail = null;
+  #head;
+  #tail;
 
   constructor() {
     this.#head = null;
@@ -61,7 +61,7 @@ class LinkedList {
 
   addToIndex(index, value) {
     const newNode = new Node(value);
-    if (index < 0 && !this.#head) {
+    if (index < 0 || !this.#head) {
       return -1;
     }
     if (index === 0) {
@@ -86,15 +86,113 @@ class LinkedList {
     return -1;
   }
 
+  hasElement(index) {
+    if (index < 0) {
+      return -1;
+    }
+    let current = this.#head;
+    let counter = 0;
+    while (current) {
+      if (counter === index) {
+        return current.val;
+      }
+      current = current.next;
+      counter++;
+    }
+    return -1;
+  }
+
+  updateElement(index, val) {
+    let current = this.#head;
+    let counter = 0;
+    while (current) {
+      if (index === counter) {
+        current.val = val;
+      }
+      current = current.next;
+      counter++;
+    }
+  }
+
+  deleteElement(index) {
+    let current = this.#head;
+    let counter = 0;
+
+    if (index < 0 && !this.#head) {
+      return;
+    }
+
+    if (index === 0) {
+      this.#head = current.next;
+      return;
+    }
+    while (current && current.next) {
+      if (counter === index - 1) {
+        current.next = current.next.next;
+      }
+      counter++;
+      current = current.next;
+    }
+  }
+
+  get length() {
+    let current = this.#head;
+    let counter = 0;
+
+    while (current) {
+      current = current.next;
+      counter++;
+    }
+
+    return counter;
+  }
+
+  findFirst(val) {
+    if (!this.#head) {
+      return;
+    }
+    let current = this.#head;
+    let counter = 0;
+    while (current) {
+      if (current.val === val) {
+        return counter;
+      }
+      current = current.next;
+      counter++;
+    }
+  }
+
+  findLast(val) {
+    if (!this.#head) {
+      return;
+    }
+    let current = this.#head;
+    let counter = 0;
+    let lastIndex = -1;
+    while (current) {
+      if (current.val === val) {
+        lastIndex = counter;
+      }
+      current = current.next;
+      counter++;
+    }
+    return lastIndex;
+  }
+
   printList() {
     if (!this.#head) {
       return;
     }
+    let temp = "";
     let curr = this.#head;
     while (curr) {
-      console.log(curr.val);
+      temp += curr.val;
+      if (curr.next) {
+        temp += " , ";
+      }
       curr = curr.next;
     }
+    console.log("{ " + temp + " }");
   }
 }
 
@@ -116,10 +214,30 @@ list.append(4);
 //-1 0 1 2 2.5 2 3 4
 
 //READ ONE OR FIND
+const foundIndex = list.hasElement(3);
+console.log("Found Index : " + foundIndex);
 
 //READ ALL
+console.log("Before delete & update");
 list.printList();
 
 //UPDATE
+list.updateElement(2, 150);
 
 //DELETE ONE
+//list.deleteElement(0);
+console.log("After delete & update");
+list.printList();
+
+//get length
+const size = list.length;
+console.log("length of list : " + size);
+
+//READ ONE => find First & find Last
+console.log("New Append ");
+list.append(150);
+list.printList();
+const foundFirst = list.findFirst(150);
+const foundLast = list.findLast(150);
+console.log("First Element : " + foundFirst);
+console.log("Second Element : " + foundLast);
